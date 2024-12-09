2a9804f00c2fa8a2ee78e8713bbc7aa9484f6fea
FreelancerMamun <mamunkhan16345@gmail.com>
2024-12-09 23:29:32 +0600

    updae

--- my-app/src/components/nav/menubar.jsx
+++ my-app/src/components/nav/menubar.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
+
+import MegaMenu from './meganenu';
+
 const MenuBar = () => {
   return (
     <div>
-      <div className=" py-4 border-t border-[#000] ">
+      <div className=" relative py-4 border-t border-[#000] border-b border-b-[#ddd] ">
         <ul className="flex items-center justify-center gap-7">
           <li className=" uppercase text-[12px] text-[#020202] cursor-pointer font-semibold tracking-widest ">
             <Link href="/">HOME</Link>
           </li>
-          <li className=" uppercase text-[12px] text-[#020202] cursor-pointer font-semibold tracking-widest">
-            <Link href="/">Shop</Link>
+          <li className=" uppercase text-[11px] text-[#020202] cursor-pointer font-semibold tracking-widest">
+            <Link href="/shops">Shop</Link>
           </li>
-          <li className=" uppercase text-[12px] text-[#020202] cursor-pointer font-semibold tracking-widest">
-            <Link href="/">Blog</Link>
+          <li className=" uppercase text-[11px] text-[#020202] cursor-pointer font-semibold tracking-widest">
+            <Link href="/blog">Blog</Link>
           </li>
-          <li className=" uppercase text-[12px] text-[#020202] cursor-pointer font-semibold tracking-widest">
-            <Link href="/">Pages</Link>
+          <li className=" uppercase text-[11px] text-[#020202] cursor-pointer font-semibold tracking-widest">
+            <Link href="#">Pages</Link>
+            <div className="absolute top-[52px] left-0 w-full -z-10 border-b border-[#020202] opacity-0 h-[400px] text-black bg-slate-100">
+              <h3>H1</h3>
+            </div>
           </li>
-          <li className=" uppercase text-[12px] text-[#020202] cursor-pointer font-semibold tracking-widest">
+          <li className=" upper+case text-[11px] text-[#020202] cursor-pointer font-semibold tracking-widest">
             <Link href={'/ategories'}>Categories</Link>
           </li>
-          <li className=" uppercase text-[12px] text-[#020202] cursor-pointer font-semibold tracking-widest">
+          <li className=" uppercase text-[11px] text-[#020202] cursor-pointer font-semibold tracking-widest">
             <Link href={'contact'}>Contacts</Link>
           </li>
         </ul>
