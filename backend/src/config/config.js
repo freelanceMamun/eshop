17715d45d7c9ac84a3a87b809e2407e2231373c7
FreelancerMamun <mamunkhan16345@gmail.com>
2024-12-21 22:43:13 +0600

    setup backend

---
+++ backend/src/config/config.js
@@ -0,0 +1,14 @@
+import { config } from 'dotenv';
+
+config();
+
+// Config Env file
+const configEnv = {
+  PORT: process.env.PORT,
+  DBURLNAME: process.env.DATABASE_URL,
+  DBNAME: process.env.DATABASE_NAME,
+  Env: process.env.envError,
+  JWTSECKey: process.env.jWT_SECRETEkEY,
+};
+
+export default configEnv;
