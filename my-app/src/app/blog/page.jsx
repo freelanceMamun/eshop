3863b53df01c005b9af53ea8b2ca07e300e535f6
FreelancerMamun <mamunkhan16345@gmail.com>
2025-01-01 23:53:36 +0600

    upate

--- my-app/src/app/blog/page.jsx
+++ my-app/src/app/blog/page.jsx
@@ -11,35 +11,13 @@ const Blogs = () => {
         <h4 className=" uppercase font-semibold text-2xl">Blog</h4>
       </div>
       <div className="container max-w-5xl m-auto">
-        <div className="">
+        <div className="flex">
           <div className="left-side">
-            <div className="">
-              <div className="">
-                <div>
-                  <img src={thumbail.src} alt="thumbail-picture" />
-                </div>
-                <div className="">
-                  <div></div>
-                  <div>
-                    <h4>Summer Collections</h4>
-                    <p>
-                      Summer is just around the corner, which means it’s time to
-                      update our wardrobes with the latest fashion trends.
-                    </p>
-                  </div>
-                  <div>
-                    <p>
-                      <a href="#" className=' uppercase text-sm'>NEWs</a>
-                      <a href="#" className=" uppercase">
-                        Read More
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Article></Article>
+            <Article></Article>
+            <Article></Article>
           </div>
-          <div className="right-side"></div>
+          <div className="right-side sticky"></div>
         </div>
       </div>
     </div>
@@ -47,3 +25,48 @@ const Blogs = () => {
 };
 
 export default Blogs;
+
+function Article() {
+  return (
+    <article>
+      <div className="border border-b-0">
+        <div className="flex">
+          <div className="w-[210px]">
+            <img
+              className="w-full h-full object-cover"
+              src={thumbail.src}
+              alt="thumbail-picture"
+            />
+          </div>
+          <div className=" py-8 px-7">
+            <div className="text-[10px] border px-2 py-1 w-max mb-3">
+              April 23, 2023
+            </div>
+            <div>
+              <h4 className="text-[23px] mb-3 uppercase font-medium">
+                Summer Collections
+              </h4>
+              <p>
+                Summer is just around the corner, which means it’s time to
+                update our wardrobes with the latest fashion trends.
+              </p>
+            </div>
+            <div className="mt-10">
+              <p className="">
+                <a href="#" className="block uppercase text-[11px] mb-3">
+                  NEWs
+                </a>
+                <a
+                  href="#"
+                  className=" text-[13px] tracking-wide font-semibold uppercase"
+                >
+                  Read More
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
