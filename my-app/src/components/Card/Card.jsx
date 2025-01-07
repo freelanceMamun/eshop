6dd2a24c4c75e34bac08334f479b0ed695e03db5
FreelancerMamun <mamunkhan16345@gmail.com>
2025-01-08 00:11:36 +0600

    update

--- my-app/src/components/Card/Card.jsx
+++ my-app/src/components/Card/Card.jsx
@@ -6,23 +6,29 @@ const Card = () => {
   return (
     <div className="border hover:border-black transition-all duration-300 ">
       <div className=" relative">
-        <div className="offer absolute top-3 right-3 z-10 border border-red-500 px-3 py-1 w-max text-[10px] ml-auto">
-          -20%
+        <div className="flex items-center gap-3 justify-end absolute top-3 z-10 right-3">
+          <div className="border-black border bg-white px-3 py-1 w-max text-[10px]">
+            <span>BEST SELLER</span>
+          </div>
+          <div className="offer  border bg-white border-red-500 px-3 py-1 w-max text-[10px]">
+            -20%
+          </div>
         </div>
-        <div className="product-picture relative">
+
+        <div className="product-picture relative overflow-hidden">
           <Image
             quality={100}
             unoptimized={true}
             width={100}
             src={pimg}
-            className="w-full h-full"
+            className="w-full h-full Frist-picture"
           ></Image>
           <Image
             quality={100}
             unoptimized={true}
             width={100}
             height={100}
-            className="w-full h-full absolute top-0 left-0"
+            className="w-full h-full second-picture absolute top-0 left-0"
             src={poImg}
           ></Image>
         </div>
@@ -33,7 +39,7 @@ const Card = () => {
         </div>
         <div className="pb-5 px-3">
           <a className="" href="#">
-            <h4 className="py-2 text-[15px] font-semibold tracking-wide">
+            <h4 className="py-2 text-[14px] font-semibold tracking-wide">
               V Logo Pocket Wool & Silk Dress
             </h4>
           </a>
