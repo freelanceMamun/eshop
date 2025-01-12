3e11081e3c7ccdf90550e1e21c873e92db19ef1c
FreelancerMamun <mamunkhan16345@gmail.com>
2025-01-12 23:44:12 +0600

    update

--- my-app/src/components/Banner/Banner.jsx
+++ my-app/src/components/Banner/Banner.jsx
@@ -26,24 +26,44 @@ const Banner = () => {
           </div>
         </div>
         <div className="">
-          <div className="border">
-            <p className="text-center">NEW</p>
+          <div
+            className="border after:bg-black after:top-0 after:left-0 after:w-full after:h-full after:opacity-40  
+            after:z-[1] after:absolute relative bg-no-repeat bg-cover bg-center py-9 px-10 h-[330px] border-black flex flex-col justify-between items-center"
+            style={{
+              backgroundImage: 'url(' + bannerImage.src + ')',
+            }}
+          >
+            <p className="text-center z-[2] relative text-white">NEW</p>
             <div className="">
-              <h4>Grand Premium Suites</h4>
+              <h4 className="text-white relative w-10/12 text-center m-auto text-4xl font-semibold  z-[2] ">
+                Grand Premium Suites
+              </h4>
             </div>
-            <div className="">
-              <button className=" uppercase">Shop now</button>
+            <div className="relative z-[2]">
+              <button className=" text-white text-[13px] uppercase">
+                Shop now
+              </button>
             </div>
           </div>
         </div>
         <div className="">
-          <div className="border">
-            <p className="text-center">NEW</p>
+          <div
+            className="border after:bg-black after:top-0 after:left-0 after:w-full after:h-full after:opacity-40  
+            after:z-[1] after:absolute relative bg-no-repeat bg-cover bg-center py-9 px-10 h-[330px] border-black flex flex-col justify-between items-center"
+            style={{
+              backgroundImage: 'url(' + bannerImage.src + ')',
+            }}
+          >
+            <p className="text-center z-[2] relative text-white">NEW</p>
             <div className="">
-              <h4>Grand Premium Suites</h4>
+              <h4 className="text-white relative w-10/12 text-center m-auto text-4xl font-semibold  z-[2] ">
+                Grand Premium Suites
+              </h4>
             </div>
-            <div className="">
-              <button className=" uppercase">Shop now</button>
+            <div className="relative z-[2]">
+              <button className=" text-white text-[13px] uppercase">
+                Shop now
+              </button>
             </div>
           </div>
         </div>
