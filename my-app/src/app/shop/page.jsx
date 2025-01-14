9d3a154c9b14e6a7270c3042a78c26b5620a77c1
FreelancerMamun <mamunkhan16345@gmail.com>
2025-01-14 23:52:32 +0600

    update

--- my-app/src/app/shop/page.jsx
+++ my-app/src/app/shop/page.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import BreadCum from '@/components/breadcum/breadcum';
+
+import CategoryThum from '@/asset/moderno-2738826553-300x300.webp';
+
 const Shop = () => {
   return (
     <div>
       <BreadCum></BreadCum>
-      <div className="">
+      <div className=" px-5">
         <div className="flex items-center justify-between">
-          <div className=''>
-            <h3 className=' uppercase'></h3>
+          <div className="">
+            <h3 className=" text-2xl font-semibold uppercase">Shop</h3>
+          </div>
+
+          <div className="grid grid-cols-4">
+            <div className="border h-[170px] w-[170px]">
+              <img
+                src={CategoryThum.src}
+                className="w-full h-full"
+                alt="catagory-picture"
+              />
+            </div>
           </div>
         </div>
       </div>
