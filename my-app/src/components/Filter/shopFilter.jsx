0a48a2b1c6ad8bceb9bf1ee9f1ebf40712172b96
FreelancerMamun <mamunkhan16345@gmail.com>
2025-01-17 00:30:36 +0600

    update

--- my-app/src/components/Filter/shopFilter.jsx
+++ my-app/src/components/Filter/shopFilter.jsx
@@ -1,10 +1,15 @@
 'use client';
 import React, { useState } from 'react';
 import { FaPlus } from 'react-icons/fa6';
-
+import { useRef } from 'react';
 const ShopFilter = () => {
   const [taggle, setTaggle] = useState(false);
   const [height, setHeight] = useState('45px');
+
+  const ref = useRef();
+  const handelToggl = () => {
+    console.log(ref.current);
+  };
 
   return (
     <div>
@@ -13,7 +18,11 @@ const ShopFilter = () => {
           style={{ height: height }}
           className={`border-b overflow-hidden transition-all duration-300 border-[#ddd]`}
         >
-          <h6 className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between">
+          <h6
+            ref={ref}
+            onClick={handelToggl}
+            className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between"
+          >
             categories
             <FaPlus></FaPlus>
           </h6>
@@ -47,9 +56,13 @@ const ShopFilter = () => {
         </div>
         <div
           style={{ height: height }}
-          className="border-b transition-all overflow-hidden duration-300 border-[#ddd]"
+          className={`border-b overflow-hidden transition-all duration-300 border-[#ddd]`}
         >
-          <h6 className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between">
+          <h6
+            ref={ref}
+            onClick={handelToggl}
+            className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between"
+          >
             categories
             <FaPlus></FaPlus>
           </h6>
@@ -83,45 +96,13 @@ const ShopFilter = () => {
         </div>
         <div
           style={{ height: height }}
-          className="border-b transition-all  overflow-hidden duration-300 border-[#ddd]"
+          className={`border-b overflow-hidden transition-all duration-300 border-[#ddd]`}
         >
-          <h6 className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between">
-            categories
-            <FaPlus></FaPlus>
-          </h6>
-          <div className="FilterList">
-            <div className="pb-4 pt-2">
-              <ul>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div
-          style={{ height: height }}
-          className="border-b transition-all overflow-hidden duration-300 border-[#ddd]"
-        >
-          <h6 className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between">
+          <h6
+            ref={ref}
+            onClick={handelToggl}
+            className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between"
+          >
             categories
             <FaPlus></FaPlus>
           </h6>
