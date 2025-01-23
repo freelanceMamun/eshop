6016cf05cc1130f9bb8da034f882c0f3034fcdd2
FreelancerMamun <mamunkhan16345@gmail.com>
2025-01-23 23:24:11 +0600

    update

--- my-app/src/middleware.js
+++ my-app/src/middleware.js
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  console.log('middlware');
-
   return NextResponse.redirect(new URL('/my-account', request.url));
 }
 
