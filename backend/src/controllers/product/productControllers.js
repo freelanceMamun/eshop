a40b9aed524f77c28f3070b50d4978e5c74d6692
FreelancerMamun <mamunkhan16345@gmail.com>
2025-02-09 22:31:37 +0600

    update

---
+++ backend/src/controllers/product/productControllers.js
@@ -0,0 +1,11 @@
+/// Create a Product Controllers
+
+const createaProduct = (request, response) => {};
+
+/// Create a Product update Controllers
+
+const updateProduct = (request, response) => {};
+
+/// Create a Product Delete Controllers
+
+const deletedProduct = (request, response) => {};
