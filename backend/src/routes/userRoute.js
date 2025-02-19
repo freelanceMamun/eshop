eadaa86e49b8835c1a2c9e46509b0c9c2a11a2bd
FreelancerMamun <mamunkhan16345@gmail.com>
2025-02-19 19:44:12 +0600

    update

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -4,13 +4,10 @@ const userRoute = express.Router();
 // --- Controllers
 import {
   createUser,
-  dashboardControllers,
   loginUser,
-  adminControllers,
   forgotePassword,
   forgoteSavePassword,
 } from '../controllers/userControllers/userControllers.js';
-
 
 // Token
 
@@ -27,27 +24,6 @@ userRoute.post('/create-user', createUser);
 
 // login user
 userRoute.post('/login', loginUser);
-
-// dashboard route in admin
-
-userRoute.post(
-  '/dashboard',
-  verifyToken,
-  authorizeRoles('customer'),
-  dashboardControllers
-);
-
-// User Dashboard GET
-userRoute.get(
-  '/dashboard',
-  verifyToken,
-  authorizeRoles('customer'),
-  (req, response) => {
-    return response.json({ message: 'welcome to get dashboard' });
-  }
-);
-
-
 
 // Forgate Password user
 
