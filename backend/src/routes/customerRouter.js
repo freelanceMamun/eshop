079edbaf0b78f7804e29752f2a1c19aa428e0338
FreelancerMamun <mamunkhan16345@gmail.com>
2025-02-20 17:04:03 +0600

    update

--- backend/src/routes/customerRouter.js
+++ backend/src/routes/customerRouter.js
@@ -1,5 +1,11 @@
 import express from 'express';
 
+import {
+  createUser,
+  loginUser,
+  forgoteSavePassword,
+} from '../controllers/userControllers/userControllers';
+
 const customer = express.Router();
 
 export default customer;
