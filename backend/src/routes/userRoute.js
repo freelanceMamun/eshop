ee3d62a715410d9afb78dece9cf68d6d04a05879
freelanceMamun <mamunkhan16345@gmail.com>
2025-02-21 23:15:22 +0600

    update

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -2,15 +2,7 @@ import express from 'express';
 
 const userRoute = express.Router();
 
-// Token
 
-import {
-  verifyToken,
-  authorizeRoles,
-  verifyAdminToken,
-} from '../auth/AuthVerifiy.js';
-
-import { forgotPasswordMiddleware } from '../middleware/middlweare.js';
 
 
 export default userRoute;
