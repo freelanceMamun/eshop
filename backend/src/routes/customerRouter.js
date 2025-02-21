20bfacd1593cda17f6dd1e30b39bd521059aa677
freelanceMamun <mamunkhan16345@gmail.com>
2025-02-21 22:15:35 +0600

    update

--- backend/src/routes/customerRouter.js
+++ backend/src/routes/customerRouter.js
@@ -4,6 +4,7 @@ import {
   createUser,
   loginUser,
   forgoteSavePassword,
+  forgotePassword
 } from '../controllers/userControllers/userControllers';
 
 const customer = express.Router();
@@ -14,6 +15,14 @@ customer.post("/create-user", createUser)
 // Login USER Route
 
 customer.post("/login", loginUser)
+
+/// Forgot Password 
+
+customer.post("/reset-password", forgotePassword)
+
+customer.post("/reset-password/email" , forgoteSavePassword)
+
+
 
 
 
