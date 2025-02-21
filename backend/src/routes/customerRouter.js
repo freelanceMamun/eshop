bca22b2613d00e58debb5dc57e477d83894022e2
freelanceMamun <mamunkhan16345@gmail.com>
2025-02-21 22:17:02 +0600

    update

--- backend/src/routes/customerRouter.js
+++ backend/src/routes/customerRouter.js
@@ -22,6 +22,18 @@ customer.post("/reset-password", forgotePassword)
 
 customer.post("/reset-password/email" , forgoteSavePassword)
 
+// logout controllers
+
+customer.get('/logout', (req, res) => {
+  // Iterate through all cookies and clear them
+  const cookies = req.cookies;
+  for (const cookieName in cookies) {
+    res.clearCookie(cookieName);
+  }
+
+  return res.json({ message: 'All cookies cleared' });
+});
+
 
 
 
--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,13 +1,6 @@
 import express from 'express';
 
 const userRoute = express.Router();
-// --- Controllers
-import {
-  createUser,
-  loginUser,
-  forgotePassword,
-  forgoteSavePassword,
-} from '../controllers/userControllers/userControllers.js';
 
 // Token
 
@@ -19,29 +12,6 @@ import {
 
 import { forgotPasswordMiddleware } from '../middleware/middlweare.js';
 
-// create user;
-userRoute.post('/create-user', createUser);
-
-// login user
-userRoute.post('/login', loginUser);
-
-// Forgate Password user
-
-userRoute.post('/reset-password', forgotPasswordMiddleware, forgotePassword);
-
-userRoute.post('/reset-password/email', forgoteSavePassword);
-
-// logout controllers
-
-userRoute.get('/logout', (req, res) => {
-  // Iterate through all cookies and clear them
-  const cookies = req.cookies;
-  for (const cookieName in cookies) {
-    res.clearCookie(cookieName);
-  }
-
-  return res.json({ message: 'All cookies cleared' });
-});
 
 export default userRoute;
 
