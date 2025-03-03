dd11468b3b4b6fae0fe6e300693229cfadecc133
freelanceMamun <mamunkhan16345@gmail.com>
2025-03-03 22:21:06 +0600

    update

--- my-app/src/components/Filter/shopFilter.jsx
+++ my-app/src/components/Filter/shopFilter.jsx
@@ -3,138 +3,72 @@
 import React, { useState } from 'react';
 import { FaPlus } from 'react-icons/fa6';
 import { useRef } from 'react';
+
+let arr = [1,2,3];
+
 const ShopFilter = () => {
   const [taggle, setTaggle] = useState(false);
   const [height, setHeight] = useState('45px');
 
   const ref = useRef();
-  const handelToggl = () => {
-    console.log(ref.current);
+  const handelToggl = (key,index) => {
+     let num =  arr.findIndex((a)=> a === key)
+     
+     if(num === index){
+      setHeight("200px")
+     }
+     
   };
 
   return (
     <div>
       <div className="px-4">
-        <div
-          style={{ height: height }}
-          className={`border-b overflow-hidden transition-all duration-300 border-[#ddd]`}
-        >
-          <h6
-            ref={ref}
-            onClick={handelToggl}
-            className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between"
-          >
-            categories
-            <FaPlus></FaPlus>
-          </h6>
-          <div className="FilterList">
-            <div className="pb-4 pt-2">
-              <ul>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div
-          style={{ height: height }}
-          className={`border-b overflow-hidden transition-all duration-300 border-[#ddd]`}
-        >
-          <h6
-            ref={ref}
-            onClick={handelToggl}
-            className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between"
-          >
-            categories
-            <FaPlus></FaPlus>
-          </h6>
-          <div className="FilterList">
-            <div className="pb-4 pt-2">
-              <ul>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div
-          style={{ height: height }}
-          className={`border-b overflow-hidden transition-all duration-300 border-[#ddd]`}
-        >
-          <h6
-            ref={ref}
-            onClick={handelToggl}
-            className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between"
-          >
-            categories
-            <FaPlus></FaPlus>
-          </h6>
-          <div className="FilterList">
-            <div className="pb-4 pt-2">
-              <ul>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-                <li className="flex pb-3">
-                  <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
-                    <input type="checkbox" />
-                    <span>Accessories</span>
-                  </p>
-                  <p></p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        {arr.map((key, index)=>{ 
+          return   <div
+         
+          key={index}
+           onClick={()=> handelToggl(key,index)}
+           style={{maxHeight : height} }
+           className={`border-b overflow-hidden transition-all duration-500 border-[#ddd]`}
+         >
+           <h6
+            
+             className=" py-3 text-[13px] cursor-pointer uppercase font-semibold tracking-wider flex justify-between"
+           >
+             categories
+             <FaPlus></FaPlus>
+           </h6>
+           <div className="FilterList">
+             <div className="pb-4 pt-2">
+               <ul>
+                 <li className="flex pb-3">
+                   <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
+                     <input type="checkbox" />
+                     <span>Accessories</span>
+                   </p>
+                   <p></p>
+                 </li>
+                 <li className="flex pb-3">
+                   <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
+                     <input type="checkbox" />
+                     <span>Accessories</span>
+                   </p>
+                   <p></p>
+                 </li>
+                 <li className="flex pb-3">
+                   <p className="flex text-[10px] uppercase tracking-widest font-semibold gap-1 items-center">
+                     <input type="checkbox" />
+                     <span>Accessories</span>
+                   </p>
+                   <p></p>
+                 </li>
+               </ul>
+             </div>
+           </div>
+         </div>
+         })}
+       
+       
       </div>
     </div>
   );
