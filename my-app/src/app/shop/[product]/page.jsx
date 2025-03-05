e2cbb321d70753db4b976b9b8c3ad4e74b22cd0b
freelanceMamun <mamunkhan16345@gmail.com>
2025-03-05 19:52:43 +0600

    update

--- my-app/src/app/shop/[product]/page.jsx
+++ my-app/src/app/shop/[product]/page.jsx
@@ -5,9 +5,6 @@ const Product = () => {
     <div className=' grid grid-cols-2'>
       <div></div>
       <div>
-
-
-      
       <div className='flex items-start'>
         {/* Bread Come */}
          <BreadCum />
@@ -25,6 +22,23 @@ const Product = () => {
 
       </div>
       {/*  Product Descript */}
+      {/* Size */}
+      <div className=''>
+        <div className='flex items-center'>
+          <h5 className=' text-[16px] font-bold uppercase'>Size</h5>
+          <h5 className=' uppercase'>Size Guide</h5>
+        </div>
+        <div className=''>
+          <div className='flex gap-1'>
+            <div className='border px-4 py-2 text-[14px] border-black'>2</div>
+            <div className='border px-4 py-2 text-[14px] border-black'>4</div>
+            <div className='border px-4 py-2 text-[14px] border-black'>6</div>
+            <div className='border px-4 py-2 text-[14px] border-black'>8</div>
+          </div>
+        </div>
+      </div>
+
+      {/* Size */}
       </div>
     </div>
   </div>;
