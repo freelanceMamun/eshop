0a9a069b8b68ea29713724a4c220e7c4de2621b0
freelanceMamun <mamunkhan16345@gmail.com>
2025-03-05 17:28:14 +0600

    update

--- my-app/src/app/shop/[product]/page.jsx
+++ my-app/src/app/shop/[product]/page.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
-
+import BreadCum from '@/components/breadcum/breadcum';
 const Product = () => {
-  return <div>Product</div>;
+  return <div>
+    <div className=' grid grid-cols-2'>
+      <div></div>
+      <div>
+
+
+      
+      <div className='flex items-start'>
+        {/* Bread Come */}
+         <BreadCum />
+        {/* Bread Come */}
+      </div>
+      {/*  Product Descript */}
+      <div className=''>
+       <div className=''>
+        <h4>Crossback Halter Dress</h4>
+        <div className=''>$2,400.00-$2,650.00</div>
+        <div>
+        A shapely halter dress designed with slinky crossback straps and a leg-baring slit is an elegant option for your upcoming event.
+        </div>
+       </div>  
+
+      </div>
+      {/*  Product Descript */}
+      </div>
+    </div>
+  </div>;
 };
 
 export default Product;
